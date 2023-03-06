88caa6fb4c89ae190209d044aa4bed7ae7f5a4d5
tomdiamond1 <tom_masters@hotmail.co.uk>
2023-03-06 13:44:44 +0000

    Calendar UI session beam filter

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -118,6 +118,37 @@ function SessionList({
   );
 }
 
+function SessionBeamLineList({
+  sessions,
+  day,
+}: {
+  sessions: Session[];
+  day: DateTime;
+}) {
+  const relevantSessions = sessions.filter((session) => {
+    const dayEnd = day.endOf('day');
+    const startTime = DateTime.fromISO(session.startDate);
+    return startTime >= day && startTime <= dayEnd;
+  });
+  return (
+    <ul>
+      {relevantSessions.map((session) => {
+        const sessionStartDate = DateTime.fromISO(session.startDate);
+        const sessionEndDate = DateTime.fromISO(session.endDate);
+        const duration = sessionEndDate.diff(sessionStartDate, 'hours');
+
+        return (
+          <li key={session.sessionId}>
+            {sessionStartDate.toFormat('HH:mm')}
+            <br />
+            {session.beamLineName}:
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 function CalendarDays({ year, month }: { year: number; month: number }) {
   const proposal = usePath('proposal');
   const sessions = useSuspense(SessionResource.getList, {
@@ -149,6 +180,10 @@ function CalendarDays({ year, month }: { year: number; month: number }) {
         >
           <h2>{day}</h2>
           <SessionList
+            sessions={sessions.results}
+            day={DateTime.fromObject({ year, month, day: day })}
+          />
+          <SessionBeamLineList
             sessions={sessions.results}
             day={DateTime.fromObject({ year, month, day: day })}
           />
